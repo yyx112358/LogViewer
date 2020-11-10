747782245a3b5fd6269f98642622eaf9a7002552
yyx112358 <yangyixuan.112358@bytedance.com>
2020-11-10 17:26:03 +0800

    用QProcess实现log输出

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QThread>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , logProcess(this)
 {
     ui->setupUi(this);
+    connect(ui->actionStart, &QAction::triggered, this, &MainWindow::Start);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::Start(bool b)
+{
+    if(b == true)
+    {
+        QStringList arguments;
+        qDebug() << arguments;
+        
+        logProcess.start("idevicesyslog", arguments);
+        if(logProcess.waitForStarted())
+        {
+            qDebug() << "logProcess started";
+            while(ui->actionStart->isChecked() == true)
+            {
+                QByteArray arr = logProcess.readLine();
+                if(!arr.isEmpty())
+                {
+                    qDebug() << arr;
+                    ui->textBrowser->insertPlainText(QString(arr));
+                    ui->textBrowser->moveCursor(QTextCursor::End);
+                }
+                QCoreApplication::processEvents();
+                QThread::msleep(1);
+            }
+        }
+    }
+    logProcess.close();
+    logProcess.waitForFinished();
+    qDebug() << "logProcess closed";
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QProcess logProcess;
+    
+    void Start(bool b);
 };
 #endif // MAINWINDOW_H
